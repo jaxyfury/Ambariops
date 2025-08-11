39a8258524684015e345d7210be5c16498d7e003
jaxy <jaxy78330@gmail.com>
2025-08-11 17:08:27 +0000

    ❯ pnpm install zsh: command not found: pnpm ❯ npm installl Unknown comma

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  transpilePackages: ['@amberops/ui', '@amberops/api', '@amberops/design-tokens'],
+};
+
+export default nextConfig;
