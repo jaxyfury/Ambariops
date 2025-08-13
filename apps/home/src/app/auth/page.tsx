59a7d3e2fab793a8b60ad71cb21adc7dbd652c31
jaxy <jaxy78330@gmail.com>
2025-08-13 09:25:19 +0000

    fix

--- apps/home/src/app/auth/page.tsx
+++ apps/home/src/app/auth/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, 'useState', 'useEffect' } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { signIn } from 'next-auth/react';
