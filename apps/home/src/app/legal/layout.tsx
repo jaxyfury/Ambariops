cf1cde3d6b78140c955545d22f73b5030587ba68
jaxy <jaxy78330@gmail.com>
2025-08-14 09:15:18 +0000

    │    Linting and checking validity of types ... │ Failed to compile. │ .

--- apps/home/src/app/legal/layout.tsx
+++ apps/home/src/app/legal/layout.tsx
@@ -1,6 +1,6 @@
 
-import { Header } from "@/components/layout/header";
-import { Footer } from "@/components/layout/footer";
+import { Header } from "../../components/layout/header";
+import { Footer } from "../../components/layout/footer";
 
 export default function LegalLayout({
   children,
