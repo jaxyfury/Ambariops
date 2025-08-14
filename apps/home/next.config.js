e987aea38b7d5e81ef7a7ff9b955e1f31c7d1d51
jaxy <jaxy78330@gmail.com>
2025-08-14 12:44:01 +0000

    nope still not woki=ing after sucesful login i can see toast message but

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -28,6 +28,12 @@ const nextConfig = {
         config.resolve.alias['@'] = path.resolve(__dirname, 'src')
         return config
     },
+    env: {
+        NEXT_PUBLIC_AUTH_API_URL: process.env.NEXT_PUBLIC_AUTH_API_URL || 'http://localhost:3002/api',
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3004/api/v1',
+        NEXT_PUBLIC_ADMIN_URL: process.env.NEXT_PUBLIC_ADMIN_URL || 'http://localhost:3003',
+        NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL || 'http://localhost:3000',
+    }
 };
 
 module.exports = nextConfig;
