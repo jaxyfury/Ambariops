951104ac0afac0de52dfa81341f9349a10955fe0
jaxy <jaxy78330@gmail.com>
2025-08-14 09:50:14 +0000

    info => Cleaning outputDir: storybook-static info => Loading presets WAR

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
@@ -16,6 +15,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ['../../apps/web/public'],
 };
 export default config;
